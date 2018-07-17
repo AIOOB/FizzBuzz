863b2292cee96349b1995eeb4960f4adc5e1f343
Jack Rickard <Jack.Rickard@softwire.com>
2018-07-17 17:01:05 +0100

    Add Fezz for multiples of 13

--- index.js
+++ index.js
@@ -13,6 +13,13 @@ for (let num = 1; num < 200; num++) {
         output.length = 0
         output.push("Bong");
     }
+    if (num % 13 == 0) {
+        if (num % 3 == 0) {
+            output.splice(1, 0, "Fezz");
+        } else {
+            output.unshift("Fezz");
+        }
+    }
     if (output.length < 1) {
         output.push(num);
     }
