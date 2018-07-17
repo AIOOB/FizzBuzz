66f93bc57cae84cb293d33ba3d6668ba81c0a320
Jack Rickard <Jack.Rickard@softwire.com>
2018-07-17 16:52:26 +0100

    Add Bong for multiples of 11

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-for (let num = 1; num < 100; num++) {
-    var output = [];
+for (let num = 1; num < 200; num++) {
+    const output = [];
     if (num % 3 == 0) {
         output.push("Fizz");
     }
@@ -7,7 +7,11 @@ for (let num = 1; num < 100; num++) {
         output.push("Buzz");
     }
     if (num % 7 == 0) {
-        output.push("Bang")
+        output.push("Bang");
+    }
+    if (num % 11 == 0) {
+        output.length = 0
+        output.push("Bong");
     }
     if (output.length < 1) {
         output.push(num);
