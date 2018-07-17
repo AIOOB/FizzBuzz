c9dc7f5bd00ec01ef2492cc1dac543b901b78593
Jack Rickard <Jack.Rickard@softwire.com>
2018-07-17 16:22:56 +0100

    FizzBuzz

---
+++ index.js
@@ -0,0 +1,13 @@
+for (let num = 1; num < 100; num++) {
+    var output = [];
+    if (num % 3 == 0) {
+        output.push("Fizz");
+    }
+    if (num % 5 == 0) {
+        output.push("Buzz");
+    }
+    if (output.length < 1) {
+        output.push(num);
+    }
+    console.log(output.join(''));
+}
