4ab23c4898c7fd5a26d049eb372d1865950a4673
Jack Rickard <jack.rickard@softwire.com>
2018-07-18 12:07:09 +0100

    Style changes + a minor fix

--- index.js
+++ index.js
@@ -3,37 +3,38 @@ const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
-rl.question("What is the maximum number you would like: ", (max) => {
-    for (let num = 1; num < max; num++) {
+
+rl.question('What is the maximum number you would like: ', (max) => {
+    for (let num = 1; num <= max; num++) {
         const output = [];
-        if (num % 3 == 0) {
-            output.push("Fizz");
-        }
-        if (num % 5 == 0) {
-            output.push("Buzz");
-        }
-        if (num % 7 == 0) {
-            output.push("Bang");
-        }
-        if (num % 11 == 0) {
+        pushIfDivisible(output, num, 3, 'Fizz');
+        pushIfDivisible(output, num, 5, 'Buzz');
+        pushIfDivisible(output, num, 7, 'Bang');
+        if (num % 11 === 0) {
             output.length = 0
-            output.push("Bong");
+            output.push('Bong');
         }
-        if (num % 13 == 0) {
-            if (num % 3 == 0) {
-                output.splice(1, 0, "Fezz");
+        if (num % 13 === 0) {
+            if (num % 3 === 0) {
+                output.splice(1, 0, 'Fezz');
             } else {
-                output.unshift("Fezz");
+                output.unshift('Fezz');
             }
         }
-        if (num % 17 == 0) {
+        if (num % 17 === 0) {
             output.reverse();
         }
-        if (output.length < 1) {
+        if (output.length === 0) {
             output.push(num);
         }
         console.log(output.join(''));
     }
 
-    rl.close;
+    rl.close();
 });
+
+function pushIfDivisible(array, number, divisor, string) {
+    if (number % divisor === 0) {
+        array.push(string);
+    }
+}
